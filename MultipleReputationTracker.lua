afc6964fcbe671cb0f698fa8fedda9359870740b
Arco Dielhof <arcodielhof@hotmail.com>
2021-05-14 21:31:21 +0200

    BG Bug Fix.

--- MultipleReputationTracker.lua
+++ MultipleReputationTracker.lua
@@ -284,15 +284,19 @@ end
 
 function AZP.MultipleReputationTracker:ShareVersion()
     local versionString = string.format("|MRT:%d|", AZP.VersionControl["Multiple Reputation Tracker"])
-    if IsInGroup() then
-        if IsInRaid() then
-            C_ChatInfo.SendAddonMessage("AZPVERSIONS", versionString ,"RAID", 1)
-        else
-            C_ChatInfo.SendAddonMessage("AZPVERSIONS", versionString ,"PARTY", 1)
+    if UnitInBattleground("player") ~= nil then
+        -- BG stuff?
+    else
+        if IsInGroup() then
+            if IsInRaid() then
+                C_ChatInfo.SendAddonMessage("AZPVERSIONS", versionString ,"RAID", 1)
+            else
+                C_ChatInfo.SendAddonMessage("AZPVERSIONS", versionString ,"PARTY", 1)
+            end
         end
-    end
-    if IsInGuild() then
-        C_ChatInfo.SendAddonMessage("AZPVERSIONS", versionString ,"GUILD", 1)
+        if IsInGuild() then
+            C_ChatInfo.SendAddonMessage("AZPVERSIONS", versionString ,"GUILD", 1)
+        end
     end
 end
 
